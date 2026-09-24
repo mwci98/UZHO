3e694ca9a598a83b9532714a5161f07ce09e0939
mwci98 <mwci98@gmail.com>
2026-09-24 21:20:51 +0530

    Revert "Add office bearers table and homepage president section"
    This reverts commit 6a9a598bfde5bc2e5d34ac1745cb84fc39b5b051.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Hero } from '../components/home/Hero';
-import { PresidentSection } from '../components/home/PresidentSection';
 import { AboutPreview } from '../components/home/AboutPreview';
 import { OurWorkSection } from '../components/home/OurWorkSection';
 import { FeaturedActivitiesSection } from '../components/home/FeaturedActivitiesSection';
@@ -15,7 +14,6 @@ export const HomePage: React.FC = () => {
     <main>
       {/* 1. Hero Section */}
       <Hero />
-      <PresidentSection />
 
       {/* 2. About Preview */}
       <AboutPreview />
